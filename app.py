6d7726fa1fa333afce37262e2aa03ce2e8d5c839
krushton <kjrushton@gmail.com>
2012-10-07 09:49:02 -0700

    fix indentation issue

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def call_api(url,params):
 	return result
 
 if __name__ == '__main__':
-	app.debug = True
-	port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        app.debug = True
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
 	
