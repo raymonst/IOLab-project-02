ee667976f66f40480c60f390241b860802e52ca3
krushton <kjrushton@gmail.com>
2012-10-18 08:54:08 -0700

    filter out stop words

--- helpers.py
+++ helpers.py
@@ -24,7 +24,6 @@ def get_hashtags_from_search(results):
 
 def get_photo_urls(user, tags):
     photo_list = []
-    tags = ','.join(tags.split())
     if user is '':
         photo_list = get_photo_list(tags, '')
     else:
@@ -52,6 +51,11 @@ def get_user_id(user):
     return call_api(FLICKR_URL, params)['user']['id']
 
 def get_photo_list(tags, id):
+    tags = tags.split(' ')
+    for tag in tags:
+        if is_stopword(tag):
+            tags.remove(tag)
+    tags = ','.join(tags)
     params = {
         'api_key': FLICKR_KEY,
         'content_type': 1,
@@ -77,6 +81,23 @@ def get_photo_byid(id):
         'nojsoncallback': 1,
     }
     return call_api(FLICKR_URL, params)
+
+def is_stopword(tag):
+    stopwords = ["a","able","about","across","after","all","almost","also","am",
+        "among","an","and","any","are","as","at","be","because","been","but","by",
+        "can","cannot","could","dear","did","do","does","either","else","ever",
+        "every","for","from","get","got","had","has","have","he","her","hers","him",
+        "his","how","however","i","if","in","into","is","it","its","just","least",
+        "let","like","likely","may","me","might","most","must","my","neither","no",
+        "nor","not","of","off","often","on","only","or","other","our","own","rather",
+        "said","say","says","she","should","since","so","some","than","that","the",
+        "their","them","then","there","these","they","this","tis","to","too","twas",
+        "us","wants","was","we","were","what","when","where","which","while","who",
+        "whom","why","will","with","would","yet","you","your" ]
+
+    if tag in stopwords:
+        return True
+    return False
 
 def call_api(url, params):
     data = urllib.urlencode(params)
